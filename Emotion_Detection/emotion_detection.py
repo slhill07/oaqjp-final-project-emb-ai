e4dfcdec32872f204ab14c278adadadda1f53d92
slhill07 <lovelyslh@yahoo.com>
2026-03-20 08:43:46 -0400

    Initial Commit

--- Emotion_Detection/emotion_detection.py
+++ Emotion_Detection/emotion_detection.py
@@ -1,30 +1,26 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyse):
-    URL= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    Input= { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(URL, json =Input, headers=Headers)
-    formatted_response = json.loads(response.text)
-    if response.status_code == 200:
-        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-        desired_object = {
-            'anger': emotion_scores['anger'],
-            'disgust': emotion_scores['disgust'],
-            'fear': emotion_scores['fear'],
-            'joy': emotion_scores['joy'],
-            'sadness': emotion_scores['sadness'],
-            'dominant_emotion': dominant_emotion
-        }
-    elif response.status_code == 400:
-        desired_object = {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None
-        }
-    return desired_object
+def emotion_detector(text_to_analyze):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=input_json, headers=header)
+    status_code = response.status_code
+
+    emotions = {}
+
+    if status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions.items(), key=lambda x: x[1])
+        emotions['dominant_emotion'] = dominant_emotion[0]
+    elif status_code == 400:
+        emotions['anger'] = None
+        emotions['disgust'] = None
+        emotions['fear'] = None
+        emotions['joy'] = None
+        emotions['sadness'] = None
+        emotions['dominant_emotion'] = None
+    return emotions
+    
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""server.py: Flask app for detecting emotions in text using the Emotion_Detection module."""
+
 from flask import Flask, request, render_template
 from Emotion_Detection.emotion_detection import emotion_detector
 
@@ -5,6 +7,8 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """Identifies emotions in text and returns the results."""
+
     text_to_analyse = request.args.get('textToAnalyze')
     emotion_result = emotion_detector(text_to_analyse)
     anger = emotion_result['anger']
@@ -14,6 +18,9 @@ def emotion_analyzer():
     sadness = emotion_result['sadness']
     dominant_emotion = emotion_result['dominant_emotion']
 
+    if dominant_emotion is None:
+        return "Invalid text! Please try again"
+
     response_str = f"""For the given statement, the system response is
     'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
     The dominant emotion is <strong>{dominant_emotion}</strong>."""
@@ -21,6 +28,7 @@ def emotion_analyzer():
 
 @app.route("/")
 def render_index_page():
+    """Renders index.html for the web app."""
     return render_template('index.html')
 
 if __name__ == "__main__":
